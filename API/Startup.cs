fd83753ddd414afaf8569e6aeefc6c342007573b
teja59300 <pemmarajusurya87@isb.tep.edu>
2020-01-04 20:41:52 +0530

    end of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,10 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => 
+            opt.AddPolicy("CorsPolicy",policy => {
+                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+            }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,13 +45,14 @@ namespace API
             {
                 app.UseDeveloperExceptionPage();
             }
-
+           
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
             app.UseAuthorization();
 
+             app.UseCors("CorsPolicy");
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
---
+++ client-app/src/App.tsx
@@ -0,0 +1,43 @@
+import React,{Component} from 'react';
+import './App.css';
+import axios from 'axios';
+import { Header, Icon, List } from 'semantic-ui-react'
+
+class App extends Component {
+  state = {
+      values : []
+  }
+  componentDidMount(){
+    axios.get('http://localhost:5000/api/values')
+    .then((response) =>{
+      // console.log(response);
+      this.setState({
+        values : response.data
+      })
+    })
+      
+    
+    
+  }
+ render()
+ {
+  return (
+    <div>
+      <Header as='h2'>
+    <Icon name='users' />
+    <Header.Content>Goal Acheivers</Header.Content>
+  </Header>
+  <List>
+   
+    {this.state.values.map((value:any) => (
+         
+         <List.Item key= {value.id}>{value.name}</List.Item>
+         ))}
+  </List>
+     
+    </div>
+  );
+ }
+}
+
+export default App;

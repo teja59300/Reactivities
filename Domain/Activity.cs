e64d6d1c13327f6c3a161c9912606a05884eb538
teja59300 <pemmarajusurya87@isb.tep.edu>
2020-01-05 16:00:36 +0530

    End of section 4

---
+++ Domain/Activity.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Domain
+{
+    public class Activity
+    {
+        public Guid Id{get;set;}
+        public string Title { get; set; }
+
+        public string Description{get;set;}
+
+        public string Categeory{get;set;}
+        public DateTime Date{get;set;}
+        public string City {get;set;}
+
+        public string Venue{get;set;}
+        
+    }
+}
